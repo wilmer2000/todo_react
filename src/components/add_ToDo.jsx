f3de26e0791cbad6c71797f2727599664a2d5a8e
Wilmer Blanco <wilmervzla2000@gmail.com>
2017-04-12 10:15:36 -0300

    fixs

--- src/components/add_ToDo.jsx
+++ src/components/add_ToDo.jsx
@@ -26,9 +26,10 @@ class AddToDo extends Component {
 
 	inputChange(event){
 		let modifiedItem = _.cloneDeep(this.state.todo)
-		let last = _.last(modifiedItem)
-		modifiedItem[last] = event.target.ỉd
-		modifiedItem[event.target.title] = event.target.title
+		console.log(modifiedItem)
+		// let last = _.last(modifiedItem)
+		// modifiedItem[last] = event.target.ỉd
+		modifiedItem[event.target.id] = event.target.value
 		this.setState({
 			todo : modifiedItem
 		})
@@ -37,18 +38,18 @@ class AddToDo extends Component {
 	itemSave(event){
 		event.preventDefault();
 		this.props.createItemToDo(this.state.todo)
-	}
-	validateItem(item){
+
 	}
 
 	render(){
+		const btnClass = this.state.todo.title === "" ? "disabled" : ""
 		return(
 			<form className="form-inline" onSubmit={this.itemSave}> 
-			  <div className="form-group">
+			  <div className="form-group cont-btn-search">
 			    <label>Title</label>
 			    <input id="title" type="text" className="form-control" onChange={this.inputChange}/>
+		  		<button type="submit" disabled={ btnClass }  className="btn btn-success" >+</button>
 			  </div>
-		  		<button type="submit" className="btn btn-default" >Submit</button>
 			</form>
 		)
 	}
